0305127532a23d6e0708d5f0d4c837d6c2fead89
Bakshish Singh <singh@Bakshishs-MacBook-Air.local>
2026-03-25 15:55:04 -0400

    ngrok, cors

--- backend/main.go
+++ backend/main.go
@@ -6,33 +6,40 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 
 	"courseshare/config"
 	"courseshare/handlers"
 	"courseshare/models"
 )
 
-
-
-
 func main() {
 
+	// Connect Database
 	config.ConnectDatabase()
 
+	// Auto migrate tables
 	config.DB.AutoMigrate(&models.Course{}, &models.Note{})
 
-r := chi.NewRouter()
+	// Router
+	r := chi.NewRouter()
 
-r.Use(middleware.Logger)
-r.Use(middleware.Recoverer)
-r.Use(middleware.StripSlashes)
+	// Middlewares
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.StripSlashes)
 
+	// CORS Middleware
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+	}))
 
-
+	// Routes
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Server is running"))
-})
-
+		w.Write([]byte("Server is running"))
+	})
 
 	// Course routes
 	r.Get("/courses", handlers.GetCourses)
@@ -44,5 +51,5 @@ r.Use(middleware.StripSlashes)
 	r.Post("/notes", handlers.CreateNote)
 
 	log.Println("Server running on :8080")
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe("0.0.0.0:8080", r)
 }
